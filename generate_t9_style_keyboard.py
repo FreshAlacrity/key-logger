968b7c4e60d2d8099a8fea0ff555e465ae3b457b
Alec <freshalacrity@gmail.com>
2023-10-03 20:34:51 -0600

    Layout generation

--- generate_t9_style_keyboard.py
+++ generate_t9_style_keyboard.py
@@ -2,11 +2,93 @@ from find_conflicts import get_conflict_data
 
 
 def generate_keymap():
+    def print_key_arr():
+        for index, keys in enumerate(key_arr):
+            print(f"Key {index + 1}: {' '.join(keys)}")
+        print("\n")
+
+    def highest_total_conflict():
+        running_max = 0
+        holds_max = ""
+        for char_a, obj in conflict_dict.items():
+            total = 0
+            for _, value in obj.items():
+                total += value
+            if total > running_max:
+                holds_max = char_a
+                running_max = total
+        return (holds_max, running_max)
+
+    def assigned_chars():
+        return [item for sublist in key_arr for item in sublist]
+
+    def conflict_product_dict(char_list):
+        """Finds the product of conflicts with the set of characters"""
+        temp_dict = {}
+        for char_a in char_list:
+            for char_b in conflict_dict[char_a]:
+                if char_b not in char_list:
+                    if char_b in temp_dict:
+                        temp_dict[char_b] *= conflict_dict[char_a][char_b] + 1
+                    else:
+                        temp_dict[char_b] = conflict_dict[char_a][char_b] + 1
+        return temp_dict
+
+    def most_conflict(char_list):
+        """Finds the character that conflicts most with the set of characters"""
+        temp_dict = conflict_product_dict(char_list)
+        heck = max(temp_dict.values())
+        return tuple(filter(lambda a: a[1] == heck, temp_dict.items()))[0]
+
+    def narrow_conflict_product(char_b, char_lists):
+        """Finds the index of the group of characters that conflicts least with this char"""
+        products_by_index = [1] * len(char_lists)
+        for index, sublist in enumerate(char_lists):
+            for char_a in sublist:
+                products_by_index[index] *= conflict_dict[char_a][char_b] + 1
+        index_of_min = products_by_index.index(min(products_by_index))
+        return index_of_min
+
+    def add_to_key(char, index=-1):
+        remaining_chars.remove(char)
+
+        if index == -1:
+            key_arr.append([char])
+        else:
+            key_arr[index].append(char)
+
+    print("Getting conflict data...")
     conflict_dict = get_conflict_data()
+
+    remaining_chars = list(conflict_dict.keys())
+
+    MAX_KEYS = 6  # Conflict values currently drop around this point
+    key_arr = []
+
+    # Put the character with the highest total conflict as Key 1
+    add_to_key(highest_total_conflict()[0], index=-1)
+
+    while len(key_arr) < MAX_KEYS:
+        # Add as a new key the key that conflicts most with all assigned keys
+        worst_bud = most_conflict(assigned_chars())
+        # print(worst_bud[1] ** (1.0 / len(assigned_chars())))
+        add_to_key(worst_bud[0], index=-1)
+
+    while len(remaining_chars) >= 1:
+        # Find the key that conflicts most with the assigned keys
+        worst_bud = most_conflict(assigned_chars())
+
+        # Figure out the least worst place to put it
+        best_place = narrow_conflict_product(worst_bud[0], key_arr)
+        add_to_key(worst_bud[0], best_place)
+
+    print_key_arr()
+    print(key_arr)
+    # @todo add to layouts.json
     
-    print(conflict_dict)
-    
-    # @todo read the wikipedia for "greedy algorithm" to see if those will help go from here
+    # @todo sort by frequency of letter press for strict entry?
+
+    # @todo Test if the relative number of presses per key are similar
 
 
 generate_keymap()
